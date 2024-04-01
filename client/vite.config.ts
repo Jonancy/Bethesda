b90c7c2cb7ad9aa71baac9e5cb1d28a124d6d2de
Gaurav <gurungg113@gmail.com>
2024-04-01 13:19:01 +0545

    haa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
+  server:{port:5000},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
